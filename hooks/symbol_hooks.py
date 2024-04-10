ddddab63d57d512ff06344eef55c6684ceb05b21
unknown <milojacquet@gmail.com>
2024-04-09 19:32:32 -0700

    Add × in leaderboards

--- hooks/symbol_hooks.py
+++ hooks/symbol_hooks.py
@@ -8,6 +8,6 @@ def on_page_content(html, **kwargs):
         # do it multiple times to compensate for overlapping regexes
         old_html = html
         html = re.sub(r'(<[^>]*?)×([^>]*?>)', r'\1'+'\ue000'+r'\2', html)
-    html = re.sub(r'×', r'<span data-replace="×">x</span>', html)
-    html = re.sub('\ue000', r'×', html)
+    html = re.sub(r'×', '<span data-replace="&times;">x</span>', html)
+    html = re.sub('\ue000', '&times;', html)
     return html
